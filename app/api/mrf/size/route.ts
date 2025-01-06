db12be335002c90929403d56b55b89714256fc06
Kibru Demeke <kibdemta@gmail.com>
2025-01-06 17:31:40 +0300

    wip

--- app/api/mrf/size/route.ts
+++ app/api/mrf/size/route.ts
@@ -19,99 +19,58 @@ export async function POST(request: Request, response: Response) {
 	const postData = await request.json();
 	const { gRecaptchaToken, mrfUrl, eins, npis, billingCodes } = postData;
 
-	let reactpchaResponseData: RecaptchaResponseData | null = null;
-	const formData = `secret=${secretKey}&response=${gRecaptchaToken}`;
-	try {
-		const res = await axios.post(
-			"https://www.google.com/recaptcha/api/siteverify",
-			formData,
-			{
-				headers: {
-					"Content-Type": "application/x-www-form-urlencoded",
-				},
-			},
+	const smallEnough = await fileSmallEnough(mrfUrl);
+	if (!smallEnough)
+		return NextResponse.json(
+			{ success: false, error: "File is too big" },
+			{ status: 401 },
 		);
-		reactpchaResponseData = res.data;
-	} catch (e) {
-		console.log("recaptcha error:", e);
-		return NextResponse.json({ success: false, error: "recaptcha error" });
-	}
+	const npiValues = npis ? `${npis}`.trim() : "";
+	const einValues = eins ? `${eins}`.trim() : "";
+	const billingCodeValues = billingCodes?.trim() || "";
+	const ipAddress = request.headers.get("x-forwarded-for");
+	const userAgent = request.headers.get("user-agent");
 
-	if (
-		reactpchaResponseData?.success &&
-		reactpchaResponseData?.score &&
-		reactpchaResponseData?.score > 0.5
-	) {
-		try {
-			if (!mrfUrl) return;
+	const reportNameData = `report-${new Date().toISOString()}-${ipAddress}`;
+	const reportName = `demo_${crypto
+		.createHash("md5")
+		.update(reportNameData)
+		.digest("hex")
+		.substring(0, 8)}`;
+	const data: ReportConfig = {
+		operations: [],
+		in_network_file_urls: [mrfUrl],
+		filter: {
+			...(einValues !== "" && {
+				provider_group_tin_value: einValues.split(","),
+			}),
+			...(npiValues !== "" && {
+				provider_group_npi: npiValues.split(","),
+			}),
+			...(billingCodeValues !== "" && {
+				billing_code: billingCodeValues.split(","),
+			}),
+		},
+		report_name: reportName,
+		bucket: process.env.AWS_S3_BUCKET?.toString(),
+		meta_data: {
+			ip_address: ipAddress,
+			user_agent: userAgent,
+		},
+	};
 
-			const smallEnough = await fileSmallEnough(mrfUrl);
-			if (!smallEnough)
-				return NextResponse.json(
-					{ success: false, error: "File is too big" },
-					{ status: 401 },
-				);
-			const npiValues = npis ? `${npis}`.trim() : "";
-			const einValues = eins ? `${eins}`.trim() : "";
-			const billingCodeValues = billingCodes?.trim() || "";
-			const ipAddress = request.headers.get("x-forwarded-for");
-			const userAgent = request.headers.get("user-agent");
+	const uploader = new S3Service();
+	await uploader.uploadFile(reportName, JSON.stringify(data));
+	// add data to the cookie
+	cookies().set(`report-${reportName}`, JSON.stringify(data), {
+		path: "/",
+		maxAge: 60 * 60 * 24 * 30,
+	});
 
-			const reportNameData = `report-${new Date().toISOString()}-${ipAddress}`;
-			const reportName = `demo_${crypto
-				.createHash("md5")
-				.update(reportNameData)
-				.digest("hex")
-				.substring(0, 8)}`;
-			const data: ReportConfig = {
-				operations: [],
-				in_network_file_urls: [mrfUrl],
-				filter: {
-					...(einValues !== "" && {
-						provider_group_tin_value: einValues.split(","),
-					}),
-					...(npiValues !== "" && {
-						provider_group_npi: npiValues.split(","),
-					}),
-					...(billingCodeValues !== "" && {
-						billing_code: billingCodeValues.split(","),
-					}),
-				},
-				report_name: reportName,
-				bucket: process.env.AWS_S3_BUCKET?.toString(),
-				meta_data: {
-					ip_address: ipAddress,
-					user_agent: userAgent,
-				},
-			};
-
-			const uploader = new S3Service();
-			await uploader.uploadFile(reportName, JSON.stringify(data));
-			// add data to the cookie
-			cookies().set(`report-${reportName}`, JSON.stringify(data), {
-				path: "/",
-				maxAge: 60 * 60 * 24 * 30,
-			});
-
-			return NextResponse.json(
-				{ success: true, reportName: reportName },
-				{ status: 202 },
-			);
-		} catch (error) {
-			console.error("Error during report placement:", error);
-			// Handle any errors that occur during the API request.
-			return NextResponse.json(
-				{ success: false, error: "Internal server error" },
-				{ status: 500 },
-			);
-		}
-	} else {
-		console.error("Error during ReCaptcha verification:");
-		return NextResponse.json({
-			success: false,
-			score: reactpchaResponseData?.score,
-		});
-	}
+	return NextResponse.json(
+		{ success: true, reportName: reportName },
+		{ status: 202 },
+	);
 }
 
 const fileSmallEnough = async (mrfUrl: string) => {
